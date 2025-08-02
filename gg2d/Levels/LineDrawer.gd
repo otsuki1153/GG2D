369082da95c5e0ca49069a885bada9b4e31b2f87
Henryy :0 <otsukiyamio@gmail.com>
2025-08-01 23:34:05 -0300

    Better look line
    Added a new color and texture for the line

--- gg2d/Levels/LineDrawer.gd
+++ gg2d/Levels/LineDrawer.gd
@@ -4,13 +4,25 @@ var dragging: bool = false
 var dragStart := Vector2.ZERO
 
 
+
 @onready var line := Line2D.new()
 @export var ball: RigidBody2D
 
 func _ready() -> void:
-	line.width = 8.0
-	#line.texture = preload("res://Assets/Point Line.png")
-	#line.set_texture_mode(Line2D.LineTextureMode.LINE_TEXTURE_NONE)
+	var gradient := Gradient.new()
+	gradient.add_point(0.0, Color(0.0, 0.0,0.0,1.0))
+	gradient.add_point(1.0, Color(0.0, 0.0,0.0,0.0))
+	line.width = 16.0
+	line.texture = preload("res://Assets/Point Line.png")
+	line.set_texture_mode(Line2D.LineTextureMode.LINE_TEXTURE_TILE)
+	line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+	line.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+	line.gradient = gradient
+	line.set_begin_cap_mode(Line2D.LineCapMode.LINE_CAP_ROUND)
+	line.set_end_cap_mode(Line2D.LINE_CAP_ROUND)
+	line.set_joint_mode(Line2D.LINE_JOINT_ROUND)
+	line.round_precision = 4
+	line.antialiased = true
 	add_child(line)
 
 func getMouseCamPosition() -> Vector2:
