31a7c7d42ad78f9b1c2f8f81f1dc19062c8720ec
Henryy :0 <107266141+otsuki1153@users.noreply.github.com>
2025-08-08 18:49:18 -0300

    some changed physics
    changed a little the physics

--- gg2d/Scenes/ball.gd
+++ gg2d/Scenes/ball.gd
@@ -6,20 +6,22 @@ extends RigidBody2D
 
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if linear_velocity.length() > maxVelocity:
+		linear_velocity = linear_velocity.normalized() * maxVelocity
+		
 	if state.get_contact_count() > 0:
 		for i in range(state.get_contact_count()):
 			var normal: Vector2 = state.get_contact_local_normal(i)
 			var velo: Vector2 = linear_velocity
 			var reflected: Vector2 = velo - 2.0 * velo.dot(normal) * normal
-			linear_velocity = reflected * 0.99
+			linear_velocity = reflected
 			
 			
 	if linear_velocity.length() > 1.0:
-		linear_velocity *= 0.98
+		var friction = 3
+		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, friction * state.get_step())
 	else:
 		linear_velocity = Vector2.ZERO
 	
-	if linear_velocity.length() > maxVelocity:
-		linear_velocity = linear_velocity.normalized() * maxVelocity
 		
 	
